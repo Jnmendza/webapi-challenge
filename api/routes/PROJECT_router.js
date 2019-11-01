e43b2d9b1c016e7e44edf290610756e76d3775ca
Jnmendza <jonatmendoza@gmail.com>
2019-11-01 10:08:29 -0700

    put and post project router working

--- api/routes/PROJECT_router.js
+++ api/routes/PROJECT_router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 // MODELS
 const projectModel = require('../../data/helpers/projectModel.js')
 
+
 // - GET - //
     // -1- //
     router.get('/', (req, res) => {
@@ -32,8 +33,47 @@ const projectModel = require('../../data/helpers/projectModel.js')
             res.status(500).json(err)
         })
     })
-// - POST - //
-// - PUT - //
+// - POST - // Project Schema are name and description
+router.post('/', (req, res) => {
+    const name = req.body.name
+    const description = req.body.description
+
+    if (!name && !description) {
+        res.status(400).json({ message: 'Please include a description and name.'})
+    } else {
+        projectModel.insert(req.body)
+        .then( actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+    }
+})
+// - PUT - // update() needs and id and changes
+// Project Schema are name and description
+router.put('/:id', (req, res) => {
+    const {id} = req.params
+    const changes = req.body
+
+    if(!changes.name && !changes.description) {
+        res.status(400).json({ message: 'Please include name and description.' })
+    } else {
+        projectModel.update(id, changes)
+        .then(updates => {
+            if(updates) {
+                res.status(200).json(updates)
+            } else {
+                res.status(404).json({ message: 'Project with specified id does not exist.' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+    }
+})
 // - DEL - //
 
 // EXPORTS
